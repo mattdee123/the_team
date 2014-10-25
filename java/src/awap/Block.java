250cbef2f581db3e9d9bb1bf33476e135f0e223c
Aakash Patel <aakpat6@gmail.com>
2014-10-25 12:42:27 -0400

    Autoformat.

--- java/src/awap/Block.java
+++ java/src/awap/Block.java
@@ -7,40 +7,39 @@ import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 
 public class Block {
-	private List<Point> offsets;
+  private List<Point> offsets;
 
-	public Block() {
-	}
+  public Block() {
+  }
 
-	public Block(List<Map<String, Integer>> offsets) {
-		this.offsets = Lists.transform(offsets,
-				new Function<Map<String, Integer>, Point>() {
-					public Point apply(Map<String, Integer> map) {
-						return new Point(map);
-					}
-				});
-	}
+  public Block(List<Map<String, Integer>> offsets) {
+    this.offsets = Lists.transform(offsets, new Function<Map<String, Integer>, Point>() {
+          public Point apply(Map<String, Integer> map) {
+            return new Point(map);
+          }
+        });
+  }
 
-	public List<Point> getOffsets() {
-		return offsets;
-	}
+  public List<Point> getOffsets() {
+    return offsets;
+  }
 
-	public void setOffsets(List<Point> offsets) {
-		this.offsets = offsets;
-	}
+  public void setOffsets(List<Point> offsets) {
+    this.offsets = offsets;
+  }
 
-	public Block rotate(final int rotations) {
-		if (rotations == 0) {
-			return this;
-		}
+  public Block rotate(final int rotations) {
+    if (rotations == 0) {
+      return this;
+    }
 
-		Block block = new Block();
-		block.setOffsets(Lists.transform(offsets, new Function<Point, Point>() {
-			public Point apply(Point p) {
-				return p.rotate(rotations);
-			}
-		}));
+    Block block = new Block();
+    block.setOffsets(Lists.transform(offsets, new Function<Point, Point>() {
+      public Point apply(Point p) {
+        return p.rotate(rotations);
+      }
+    }));
 
-		return block;
-	}
+    return block;
+  }
 }
--- java/src/awap/Game.java
+++ java/src/awap/Game.java
@@ -1,85 +1,79 @@
 package awap;
 
+import com.google.common.base.Optional;
 import java.util.List;
 
-import com.google.common.base.Optional;
-
 public class Game {
-	private State state;
-	private Integer number;
+  private State state;
+  private Integer number;
 
-	public Optional<Move> updateState(State newState) {
-		if (newState.getError().isPresent()) {
-			Logger.log(newState.getError().get());
-			return Optional.absent();
-		}
+  public Optional<Move> updateState(State newState) {
+    if (newState.getError().isPresent()) {
+      Logger.log(newState.getError().get());
+      return Optional.absent();
+    }
 
-		if (newState.getMove() != -1) {
-			return Optional.fromNullable(findMove());
-		}
+    if (newState.getMove() != -1) {
+      return Optional.fromNullable(findMove());
+    }
 
-		state = newState;
-		if (newState.getNumber().isPresent()) {
-			number = newState.getNumber().get();
-		}
+    state = newState;
+    if (newState.getNumber().isPresent()) {
+      number = newState.getNumber().get();
+    }
 
-		return Optional.absent();
-	}
+    return Optional.absent();
+  }
 
-	private Move findMove() {
-		int N = state.getDimension();
-		List<Block> blocks = state.getBlocks().get(number);
+  private Move findMove() {
+    int N = state.getDimension();
+    List<Block> blocks = state.getBlocks().get(number);
 
-		for (int x = 0; x < N; x++) {
-			for (int y = 0; y < N; y++) {
-				for (int rot = 0; rot < 4; rot++) {
-					for (int i = 0; i < blocks.size(); i++) {
-						if (canPlace(blocks.get(i).rotate(rot), new Point(x, y))) {
-							return new Move(i, rot, x, y);
-						}
-					}
-				}
-			}
-		}
+    for (int x = 0; x < N; x++) {
+      for (int y = 0; y < N; y++) {
+        for (int rot = 0; rot < 4; rot++) {
+          for (int i = 0; i < blocks.size(); i++) {
+            if (canPlace(blocks.get(i).rotate(rot), new Point(x, y))) {
+              return new Move(i, rot, x, y);
+            }
+          }
+        }
+      }
+    }
 
-		return new Move(0, 0, 0, 0);
-	}
+    return new Move(0, 0, 0, 0);
+  }
 
-	private int getPos(int x, int y) {
-		return state.getBoard().get(x).get(y);
-	}
+  private int getPos(int x, int y) {
+    return state.getBoard().get(x).get(y);
+  }
 
-	private boolean canPlace(Block block, Point p) {
-		boolean onAbsCorner = false, onRelCorner = false;
-		int N = state.getDimension() - 1;
+  private boolean canPlace(Block block, Point p) {
+    boolean onAbsCorner = false, onRelCorner = false;
+    int N = state.getDimension() - 1;
 
-		Point[] corners = { new Point(0, 0), new Point(N, 0), new Point(N, N),
-				new Point(0, N) };
-		;
-		Point corner = corners[number];
+    Point[] corners = {new Point(0, 0), new Point(N, 0), new Point(N, N), new Point(0, N)};
 
-		for (Point offset : block.getOffsets()) {
-			Point q = offset.add(p);
-			int x = q.getX(), y = q.getY();
+    Point corner = corners[number];
 
-			if (x > N || x < 0 || y < 0 || y > N
-          || getPos(x, y) >= 0
-          || getPos(x, y) == -2
-					|| (x > 0 && getPos(x - 1, y) == number)
-					|| (y > 0 && getPos(x, y - 1) == number)
-					|| (x < N && getPos(x + 1, y) == number)
-					|| (y < N && getPos(x, y + 1) == number)) {
-				return false;
-			}
+    for (Point offset : block.getOffsets()) {
+      Point q = offset.add(p);
+      int x = q.getX(), y = q.getY();
 
-			onAbsCorner = onAbsCorner || q.equals(corner);
-			onRelCorner = onRelCorner
-					|| (x > 0 && y > 0 && getPos(x - 1, y - 1) == number)
-					|| (x < N && y > 0 && getPos(x + 1, y - 1) == number)
-					|| (x > 0 && y < N && getPos(x - 1, y + 1) == number)
-					|| (x < N && y < N && getPos(x + 1, y + 1) == number);
-		}
+      if (x > N || x < 0 || y < 0 || y > N || getPos(x, y) >= 0 || getPos(x, y) == -2 ||
+          (x > 0 && getPos(x - 1, y) == number) || (y > 0 && getPos(x, y - 1) == number) ||
+          (x < N && getPos(x + 1, y) == number) || (y < N && getPos(x, y + 1) == number)) {
+        return false;
+      }
 
-		return !((getPos(corner.getX(), corner.getY()) < 0 && !onAbsCorner) || (!onAbsCorner && !onRelCorner));
-	}
+      onAbsCorner = onAbsCorner || q.equals(corner);
+      onRelCorner = onRelCorner || (x > 0 && y > 0 && getPos(x - 1, y - 1) == number) ||
+          (x < N && y > 0 && getPos(x + 1, y - 1) == number) ||
+          (x > 0 && y < N && getPos(x - 1, y + 1) == number) ||
+          (x < N && y < N && getPos(x + 1, y + 1) == number);
+    }
+
+    return !((getPos(corner.getX(), corner.getY()) < 0 && !onAbsCorner) ||
+        (!onAbsCorner && !onRelCorner));
+  }
 }
